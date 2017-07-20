ddda4a96f161ce728a07d2a8e5e13e708791c8d4
czfshine <czfshine@outlook.com>
2017-07-20 22:48:14 +0800

    init freamwork

---
+++ src/component/placeholder.jsx
@@ -0,0 +1,132 @@
+import React, { Component } from 'react';
+
+class Placeholder extends  Component{
+    render(){
+        return (<div>
+                <h1 className="page-header">Dashboard</h1>
+
+                <div className="row placeholders">
+                    <div className="col-xs-6 col-sm-3 placeholder">
+                        <h4>Label</h4>
+                        <span className="text-muted">Something else</span>
+                    </div>
+                    <div className="col-xs-6 col-sm-3 placeholder">
+                        <h4>Label</h4>
+                        <span className="text-muted">Something else</span>
+                    </div>
+                    <div className="col-xs-6 col-sm-3 placeholder">
+                        <h4>Label</h4>
+                        <span className="text-muted">Something else</span>
+                    </div>
+                    <div className="col-xs-6 col-sm-3 placeholder">
+                        <h4>Label</h4>
+                        <span className="text-muted">Something else</span>
+                    </div>
+                </div>
+
+                <h2 className="sub-header">Section title</h2>
+                <div className="table-responsive">
+                    <table className="table table-striped">
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Header</th>
+                            <th>Header</th>
+                            <th>Header</th>
+                            <th>Header</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>1,001</td>
+                            <td>Lorem</td>
+                            <td>ipsum</td>
+                            <td>dolor</td>
+                            <td>sit</td>
+                        </tr>
+                        <tr>
+                            <td>1,002</td>
+                            <td>amet</td>
+                            <td>consectetur</td>
+                            <td>adipiscing</td>
+                            <td>elit</td>
+                        </tr>
+                        <tr>
+                            <td>1,003</td>
+                            <td>Integer</td>
+                            <td>nec</td>
+                            <td>odio</td>
+                            <td>Praesent</td>
+                        </tr>
+                        <tr>
+                            <td>1,003</td>
+                            <td>Integer</td>
+                            <td>nec</td>
+                            <td>odio</td>
+                            <td>Praesent</td>
+                        </tr>
+                        <tr>
+                            <td>1,003</td>
+                            <td>Integer</td>
+                            <td>nec</td>
+                            <td>odio</td>
+                            <td>Praesent</td>
+                        </tr>
+                        <tr>
+                            <td>1,003</td>
+                            <td>Integer</td>
+                            <td>nec</td>
+                            <td>odio</td>
+                            <td>Praesent</td>
+                        </tr>
+                        <tr>
+                            <td>1,003</td>
+                            <td>Integer</td>
+                            <td>nec</td>
+                            <td>odio</td>
+                            <td>Praesent</td>
+                        </tr>
+                        <tr>
+                            <td>1,003</td>
+                            <td>Integer</td>
+                            <td>nec</td>
+                            <td>odio</td>
+                            <td>Praesent</td>
+                        </tr>
+                        <tr>
+                            <td>1,003</td>
+                            <td>Integer</td>
+                            <td>nec</td>
+                            <td>odio</td>
+                            <td>Praesent</td>
+                        </tr>
+                        <tr>
+                            <td>1,003</td>
+                            <td>Integer</td>
+                            <td>nec</td>
+                            <td>odio</td>
+                            <td>Praesent</td>
+                        </tr>
+                        <tr>
+                            <td>1,003</td>
+                            <td>Integer</td>
+                            <td>nec</td>
+                            <td>odio</td>
+                            <td>Praesent</td>
+                        </tr>
+                        <tr>
+                            <td>1,003</td>
+                            <td>Integer</td>
+                            <td>nec</td>
+                            <td>odio</td>
+                            <td>Praesent</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        )
+    }
+}
+export default Placeholder;
+
