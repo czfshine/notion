c9296682dfc2ee41b79448f74ca6c59f29ee291f
czfshine <czfshine@outlook.com>
2017-07-29 17:36:09 +0800

    增加sass支持

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import "jquery/dist/jquery.min"
-import '../App.css';
+import '../App.scss';
 import "bootstrap/dist/css/bootstrap.min.css"
 import  "../dashboard.css"
 
