30e54058126d58ae4339218cb4c63b64817e198b
czfshine <czfshine@outlook.com>
2017-07-31 21:14:02 +0800

    增加vscode debug支持

--- stories/index.js
+++ stories/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import reducer from '../src/reducers';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
 import HeadBar from "../src/components/headbar.jsx"
 import '../src/style/index.css';
 import 'react-mdl/extra/material.css';
