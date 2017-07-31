d070e0b163309721f7d2694f89ee235386d0197d
czfshine <czfshine@outlook.com>
2017-07-31 15:40:51 +0800

    增加本地服务

---
+++ server.js
@@ -0,0 +1,14 @@
+/**
+ * Created by czfshine on 2017/7/31.
+ */
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(9000);
---
+++ src/containers/block.jsx
@@ -0,0 +1,19 @@
+/**
+ * Created by czfshine on 2017/7/31.
+ */
+
+import React,{Component} from 'react';
+
+class Block extends Component{
+
+
+    render(){
+
+        return (
+            <div className="block">
+            </div>
+        )
+    }
+}
+
+export  default Block;
